4523fcf408744cee8a96db7d93fb036d07462ff2
Tyler <tylermatthew.newman@gmail.com>
2026-01-14 13:43:59 -0800

    Create function getComputerChoice
    This function will generate random choice of
    RPS to play against user inputted choice.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    console.log(num)
+    if (num === 0) {
+        return 'Rock';
+    } else if (num === 1) {
+        return 'Paper';
+    } else if (num === 2) {
+        return 'Scissors';
+    }
+}
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+
