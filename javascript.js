052336010e6fd1403a8e7e701fe4a1c8c4ddfd85
Tyler <tylermatthew.newman@gmail.com>
2026-03-17 13:58:05 -0700

    Play game for 5 rounds and add option to restart
    Added logic that will have game run til a player
    has 5 wins, and then it will ask to restart game.
    Logic is untested and may need revision.

--- javascript.js
+++ javascript.js
@@ -12,45 +12,80 @@ function getComputerChoice() {
 }
 
     function playRound(humanChoice, computerChoice) {
+        winner = ''
         if (humanChoice === 'Paper'){
             if (computerChoice === 'Rock'){
-                humanScore += 1
-                console.log('You win! Paper beats Rock.')
+                winner = 'human';
+                document.getElementById('roundMessage').textContent = 'Player chose Paper and Computer chose Rock. Player wins!';
             } else if (computerChoice === 'Paper'){
-                console.log('Tie! Paper and Paper chosen.')
+                winner = 'tie';
+                document.getElementById('roundMessage').textContent = 'Both chose Paper. It is a tie!';
             } else if (computerChoice === 'Scissors') {
-                computerScore += 1
-                console.log('You lose! Scissors beats Paper.')
+                winner = 'computer';
+                document.getElementById('roundMessage').textContent = 'Player chose Paper and Computer chose Scissors. Computer wins!'
             }
         } else if (humanChoice === 'Rock'){
             if (computerChoice === 'Rock'){
-                console.log('Tie! Rock and Rock chosen.')
+                winner = 'tie';
+                document.getElementById('roundMessage').textContent = 'Both chose Rock. It is a tie!';
             } else if (computerChoice === 'Paper'){
-                computerScore += 1
-                console.log('You lose! Paper beats Rock.')
+                winner = 'computer';
+                document.getElementById('roundMessage').textContent = 'Player chose Rock and Computer chose Paper. Computer wins!'
             } else if (computerChoice === 'Scissors') {
-                humanScore += 1
-                console.log('You win! Rock beats Scissors.')
+                winner = 'human';
+                document.getElementById('roundMessage').textContent = 'Player chose Rock and Computer chose Scissors. Player wins!'
             }
         } else if (humanChoice === 'Scissors'){
             if (computerChoice === 'Rock'){
-                computerScore += 1
-                console.log('You lose! Rock beats Scissors.')
+                winner = 'computer';
+                document.getElementById('roundMessage').textContent = 'Player chose Scissors and Computer chose Rock. Computer wins!'
             } else if (computerChoice === 'Paper'){
-                humanScore += 1
-                console.log('You win! Scissors beats Paper.')
+                winner = 'human';
+                document.getElementById('roundMessage').textContent = 'Player chose Scissors and Computer chose Paper. Player wins!'
             } else if (computerChoice === 'Scissors') {
-                console.log('Tie! Scissors and Scissors chosen.')
+                winner = 'tie';
+                document.getElementById('roundMessage').textContent = 'Both chose Scissors. It is a tie!';
             }
         }
+        return winner;
     }
 
-
 const buttons = document.querySelector('#buttons');
+humanScore = 0
+computerScore = 0
 buttons.addEventListener('click', (e)=> {
     const humanChoice = e.target.id;
     const computerChoice = getComputerChoice();
-    console.log(humanChoice)
-    console.log(computerChoice)
-    playRound(humanChoice, computerChoice);
+    winner = playRound(humanChoice, computerChoice);
+    if ((humanScore & computerScore) < 4){
+        switch(winner){
+            case 'human': humanScore += 1;
+                document.getElementById('score').textContent = 'Player: ${humanScore} vs Computer: ${computerScore}';
+                if (humanScore == 5) {
+                    document.getElementById('winnerMessage').textContent = 'You win!';
+                    const playAgain = document.createElement('button');
+                    playAgain.textContent('Play Again?');
+                    document.getElementById('winnerMessage').appendChild(playAgain);
+                    playAgain.addEventListener('click', () => {
+                        window.location.reload();
+                    })
+                }
+                break;
+            case 'computer': computerScore += 1;
+                document.getElementById('score').textContent = 'Player: ${humanScore} vs Computer: ${computerScore}';
+                if (computerScore == 5) {
+                    document.getElementById('winnerMessage').textContent = 'You lose!';
+                    const playAgain = document.createElement('button');
+                    playAgain.textContent('Play Again?');
+                    document.getElementById('winnerMessage').appendChild(playAgain);
+                    playAgain.addEventListener('click', () => {
+                        window.location.reload();
+                    })
+                }
+                break;
+        }
+
+    } else{
+        document.getElementById('winnerMessage').textContent = ''
+    }
 });
